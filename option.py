2baa043a0b07217ca7197dd7dcc0011e56c13cb0
unknown <retailcapital9@gmail.com>
2023-03-11 17:28:41 -0500

    v1

--- option.py
+++ option.py
@@ -46,75 +46,102 @@ def get_calls(ticker, option_expiry):
 
 def plot_max_pain(ticker, option_date):
 
-	nq = yf.Ticker(ticker)
-	option_chain = nq.option_chain(option_date)
-	calls = option_chain.calls
-	puts = option_chain.puts
+	try:
+		nq = yf.Ticker(ticker)
+		option_chain = nq.option_chain(option_date)
+		calls = option_chain.calls
+		puts = option_chain.puts
+		merged = pd.merge(calls, puts, on='strike', suffixes=('_call', '_put'))
 
-	# Extract strike prices and open interest
-	call_strikes = np.array(calls['strike'])
-	call_oi = np.array(calls['openInterest'])
-	put_strikes = np.array(puts['strike'])
-	put_oi = np.array(puts['openInterest'])
+		# Extract strike prices and open interest
+		call_strikes = np.array(merged['strike'])
+		call_oi = np.array(merged['openInterest_call'])
+		put_strikes = np.array(merged['strike'])
+		put_oi = np.array(merged['openInterest_put'])
 
-	# Calculate total value of calls and puts for each strike price
-	underlying_price = nq.history().iloc[-1]['Close']
-	call_values = (np.maximum(call_strikes - underlying_price, 0) * call_oi)
-	put_values = (np.maximum(underlying_price - put_strikes, 0) * put_oi)
-	call_values =np.cumsum(call_values)
-	put_values=np.cumsum(put_values[::-1])[::-1]
+		# Calculate total value of calls and puts for each strike price
+		underlying_price = nq.history().iloc[-1]['Close']
+		call_values = (np.maximum(call_strikes, 0) * call_oi * 100)
+		put_values = (np.maximum(put_strikes, 0) * put_oi * 100) 
+		call_values =np.cumsum(call_values)
+		put_values=np.cumsum(put_values[::-1])[::-1]
 
-	# Create a mountain chart
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=call_strikes, y=call_values, mode='lines', fill='tozeroy', line=dict(color='green'), name='Calls'))
-	fig.add_trace(go.Scatter(x=put_strikes, y=put_values, mode='lines', fill='tozeroy', line=dict(color='red'), name='Puts'))
+		call_minus_put = call_values - put_values
+		index_maxpain = next((i for i, n in enumerate(call_minus_put) if n > 0), None)
+		max_pain = call_strikes[index_maxpain]
+		prior_max_pain = call_strikes[index_maxpain-1]
+		# Create a mountain chart
+		fig = go.Figure()
+		fig.add_trace(go.Scatter(x=call_strikes, y=call_values, mode='lines', fill='tozeroy', line=dict(color='green'), name='Calls'))
+		fig.add_trace(go.Scatter(x=put_strikes, y=put_values, mode='lines', fill='tozeroy', line=dict(color='red'), name='Puts'))
 
-	# Add labels and title
-	# fig.update_layout(title=f'{ticker} Max Pain on {option_date}', xaxis_title='Strike Price', yaxis_title='Option Value ($)')
-	fig.update_layout(
-		title={
-		'text':f'${ticker.upper()} Max Pain on {option_date}',
-		"x": 0.5,
-		"xanchor": "center",
-		"yanchor": "top",
-		"font":{"size":22}
-		}, 
-		xaxis_title={
-		'text':'<b>Strike</b>',
-		"font": {"size": 18}
-		}, 
-		yaxis_title={
-		'text':'<b>Option Value ($)</b>',
-		'font': {'size':18}
-		})
+		# Add labels and title
+		# fig.update_layout(title=f'{ticker} Max Pain on {option_date}', xaxis_title='Strike Price', yaxis_title='Option Value ($)')
+		fig.update_layout(
+			title={
+			'text':f'${ticker.upper()} Max Pain on {option_date} = ${max_pain}',
+			"x": 0.5,
+			"xanchor": "center",
+			"yanchor": "top",
+			"font":{"size":22}
+			}, 
+			xaxis_title={
+			'text':'<b>Strike</b>',
+			"font": {"size": 18}
+			}, 
+			yaxis_title={
+			'text':'<b>Option Value ($)</b>',
+			'font': {'size':18}
+			})
 
-	fig.update_layout(
-		plot_bgcolor='#1C1F2E',
-		paper_bgcolor='#1C1F2E',
-		# plot_bgcolor_border	='khaki',
-		template='plotly_dark',
-		# bordercolor='orange',
-		xaxis={
-		"tickangle": -45,
-		"tickfont": {"size": 16},
-		"showgrid":False,
-		"zeroline":False,
-		"zerolinecolor":'white',
-		"zerolinewidth":2,
-		},
+		fig.update_layout(
+			plot_bgcolor='#1C1F2E',
+			paper_bgcolor='#1C1F2E',
+			# plot_bgcolor_border	='khaki',
+			template='plotly_dark',
+			# bordercolor='orange',
+			xaxis={
+			"tickangle": -45,
+			"tickfont": {"size": 16},
+			"showgrid":False,
+			"zeroline":False,
+			"zerolinecolor":'white',
+			"zerolinewidth":2,
+			},
 
-		yaxis={
-		"tickangle": 0,
-		"tickfont": {"size": 16},
-		"showgrid":False,
-		"zeroline":False	,
-		"zerolinecolor":'white',
-		"zerolinewidth":2,
-		}
-		)
-	fig.update_layout(xaxis_range=[put_strikes.min(), call_strikes.max()])
-	st.plotly_chart(fig, use_container_width=True)
-
+			yaxis={
+			"tickangle": 0,
+			"tickfont": {"size": 16},
+			"showgrid":False,
+			"zeroline":False	,
+			"zerolinecolor":'white',
+			"zerolinewidth":2,
+			}
+			)
+		fig.add_annotation(
+			x=(max_pain + prior_max_pain)/2, y=call_values[index_maxpain-1],
+			ax=0, 
+		    # ay=0,
+		    # arrowhead=2,
+		    # arrowsize=5,
+		    text=f'${max_pain}',
+		    # showarrow=True,
+		    font=dict(
+		    	family="Courier New, monospace",
+		    	size=18,
+		    	color="yellow",
+		        # weight="bold"
+		        ),
+		    # align='center',
+		    # arrowcolor='yellow',
+		    # bgcolor='red',
+		    opacity=1
+		    )
+		fig.update_layout(xaxis_range=[put_strikes.min(), call_strikes.max()])
+		st.plotly_chart(fig, use_container_width=True)
+	except Exception as e:
+		print('Ticker not working...')
+		st.markdown("<div style='background-color: purple; padding: 20px; border-radius: 5px; color: white;'><p> This ticker doesn't exist in the database.</p></div>", unsafe_allow_html=True)
 
 
 def plot_double_bar_chart(option, ticker):
@@ -310,7 +337,7 @@ def plot_all_expiry(ticker):
 		print('Ticker not working...')
 
 def main():
-    st.write(f"<h1 style='font-size: 26px;'>Retail Capital\'s Open Interest by Strike</h1>", unsafe_allow_html=True)
+    # st.write(f"<h1 style='font-size: 26px;'>Retail Capital\'s Open Interest by Strike</h1>", unsafe_allow_html=True)
 
     col1, col2 = st.columns([1, 3])
 
@@ -330,11 +357,12 @@ def main():
 	    with col2:
 	 	   st.markdown("<a href='https://apextraderfunding.com/member/aff/go/itsonlymoney?c=GVXZJABB' target='_blank' style='color: white; display: block; text-align: center; width: 100%; background-color: #567BF4; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-weight: bold;'>APEX PROMO | Use 'GVXZJABB'</a>", unsafe_allow_html=True)
 
+	 	# st.write("Option Expiry is in beta mode", unsafe_allow_html=True)
 	    plot_max_pain(ticker,selected_option)
 	    plot_all_expiry(ticker)	    	
 
 
-    st.markdown("<div style='text-align: justify; padding: 20px; border-radius: 5px; color: white;'><p> The data presented in this app is sourced from Yahoo Finance and have delays. It's recommended to use this tool after market close. Please note that if the ticker entered is not found, no graphs will appear. Also, please avoid using the $ symbol when entering the ticker symbol. There is no need to use capital letters for the ticker.The first graph will pop up fairly quickly. The second one takes a bit longer (15s to 30s).<p>Example: QQQ, SPY, TSLA, AAPL, GOOGL... </p> </p> <p>Disclaimer: The information provided on this website is for educational purposes only and should not be construed as financial advice. The content and data presented may not be accurate or complete, and should not be relied upon for making investment decisions. You should conduct your own research and consult with a qualified financial advisor before making any investment decisions. The authors and publishers of this website are not responsible for any losses that may result from the use of this information.</p></div>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: justify; padding: 20px; border-radius: 5px; color: white;'><p> The data presented in this app is sourced from Yahoo Finance and have delays. It's recommended to use this tool after market close. Please note that if the ticker entered is not found, no graphs will appear. Also, please avoid using the $ symbol when entering the ticker symbol. There is no need to use capital letters for the ticker.The first graph will pop up fairly quickly. The second one takes a bit longer (15s to 30s). I am aware of some issues with the Yahoo Option Chain, especially for $TSLA, $GOOGL and a few other stocks, probably due to stock splits.<p>Example: QQQ, SPY, TSLA, AAPL, GOOGL... </p> </p> <p>Disclaimer: The information provided on this website is for educational purposes only and should not be construed as financial advice. The content and data presented may not be accurate or complete, and should not be relied upon for making investment decisions. You should conduct your own research and consult with a qualified financial advisor before making any investment decisions. The authors and publishers of this website are not responsible for any losses that may result from the use of this information.</p></div>", unsafe_allow_html=True)
 
 st.markdown(
 	"""

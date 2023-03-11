78c0e3d19d4c923e6290690b945aa0968bc7ccf8
unknown <retailcapital9@gmail.com>
2023-03-11 14:20:09 -0500

    new layout button

--- option.py
+++ option.py
@@ -44,6 +44,78 @@ def get_calls(ticker, option_expiry):
 		# st.exception(e)
 		print('Ticker not working...')
 
+def plot_max_pain(ticker, option_date):
+
+	nq = yf.Ticker(ticker)
+	option_chain = nq.option_chain(option_date)
+	calls = option_chain.calls
+	puts = option_chain.puts
+
+	# Extract strike prices and open interest
+	call_strikes = np.array(calls['strike'])
+	call_oi = np.array(calls['openInterest'])
+	put_strikes = np.array(puts['strike'])
+	put_oi = np.array(puts['openInterest'])
+
+	# Calculate total value of calls and puts for each strike price
+	underlying_price = nq.history().iloc[-1]['Close']
+	call_values = (np.maximum(call_strikes - underlying_price, 0) * call_oi)
+	put_values = (np.maximum(underlying_price - put_strikes, 0) * put_oi)
+	call_values =np.cumsum(call_values)
+	put_values=np.cumsum(put_values[::-1])[::-1]
+
+	# Create a mountain chart
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=call_strikes, y=call_values, mode='lines', fill='tozeroy', line=dict(color='green'), name='Calls'))
+	fig.add_trace(go.Scatter(x=put_strikes, y=put_values, mode='lines', fill='tozeroy', line=dict(color='red'), name='Puts'))
+
+	# Add labels and title
+	# fig.update_layout(title=f'{ticker} Max Pain on {option_date}', xaxis_title='Strike Price', yaxis_title='Option Value ($)')
+	fig.update_layout(
+		title={
+		'text':f'${ticker.upper()} Max Pain on {option_date}',
+		"x": 0.5,
+		"xanchor": "center",
+		"yanchor": "top",
+		"font":{"size":22}
+		}, 
+		xaxis_title={
+		'text':'<b>Strike</b>',
+		"font": {"size": 18}
+		}, 
+		yaxis_title={
+		'text':'<b>Option Value ($)</b>',
+		'font': {'size':18}
+		})
+
+	fig.update_layout(
+		plot_bgcolor='#1C1F2E',
+		paper_bgcolor='#1C1F2E',
+		# plot_bgcolor_border	='khaki',
+		template='plotly_dark',
+		# bordercolor='orange',
+		xaxis={
+		"tickangle": -45,
+		"tickfont": {"size": 16},
+		"showgrid":False,
+		"zeroline":False,
+		"zerolinecolor":'white',
+		"zerolinewidth":2,
+		},
+
+		yaxis={
+		"tickangle": 0,
+		"tickfont": {"size": 16},
+		"showgrid":False,
+		"zeroline":False	,
+		"zerolinecolor":'white',
+		"zerolinewidth":2,
+		}
+		)
+	fig.update_layout(xaxis_range=[put_strikes.min(), call_strikes.max()])
+	st.plotly_chart(fig, use_container_width=True)
+
+
 
 def plot_double_bar_chart(option, ticker):
 
@@ -53,11 +125,12 @@ def plot_double_bar_chart(option, ticker):
 
 	    df_calls = option_data[0]
 	    df_puts = option_data[1]
+	    # df_puts['openInterest'] = df_puts['openInterest'].apply(lambda x: -x)
 	    last_close = option_data[2]
 
 	    fig = go.Figure(data=[
 	        go.Bar(name='Calls OI', x=df_calls['strike'], y=df_calls['openInterest'], marker=dict(color='green')),
-	        go.Bar(name='Puts OI', x=df_puts['strike'], y=df_puts['openInterest'], marker=dict(color='#FF5757'))
+	        go.Bar(name='Puts OI', x=df_puts['strike'], y=df_puts['openInterest'], marker=dict(color='#FF5757', opacity=0.8))
 	    ])
 	    fig.update_layout(barmode='group', 
 	    	title={
@@ -101,11 +174,13 @@ def plot_double_bar_chart(option, ticker):
 		        "tickangle": 0,
 		        "tickfont": {"size": 16},
 		        "showgrid":False,
-		        "zeroline":False,
+		        "zeroline":False	,
 		        "zerolinecolor":'white',
 		        "zerolinewidth":2,
 	    	}
 	    	)
+	    # if len(df_puts) > 100:
+	    	# fig.update_layout(xaxis_range=[max(last_close*0.65,df_puts['strike'].min()),min(last_close*1.35, df_calls['strike'].max())])
 
 	    st.plotly_chart(fig, use_container_width=True)
 
@@ -174,6 +249,7 @@ def plot_all_expiry(ticker):
 
 		all_puts = pd.DataFrame.from_dict(all_puts, orient='index').reset_index()
 		all_puts.columns = ['Expiry', 'openInterest']
+		# all_puts['Expiry'] = all_puts['Expiry'].apply(lambda x: -x)
 
 		fig = go.Figure(data=[
 			go.Bar(name='Calls OI', x=all_calls['Expiry'], y=all_calls['openInterest'], marker=dict(color='green')),
@@ -236,21 +312,13 @@ def plot_all_expiry(ticker):
 def main():
     st.write(f"<h1 style='font-size: 26px;'>Retail Capital\'s Open Interest by Strike</h1>", unsafe_allow_html=True)
 
-    # col1, col2 = st.columns(2)
-    # ticker = col1.columns(2)[0].text_input('Enter a Ticker')
     col1, col2 = st.columns([1, 3])
 
-    # ticker = st.text_input('Enter a Ticker', 'QQQ').strip().title()
     ticker = col1.text_input('Enter a Ticker', 'QQQ').strip().title()
 
     if ticker != '':
 	    option_expiry = get_expiry(ticker)
-	    # selected_option = col1.columns(2)[0].selectbox('Select an Option Expiry', option_expiry)
 	    selected_option = col2.selectbox('Select an Option Expiry', option_expiry)
-
-	    # col1.columns([1, 3])
-	    # col1.columns(2)[0].width = 1
-	    # col1.columns(2)[1].width = 1
 
 	    plot_double_bar_chart(selected_option, ticker)
 
@@ -262,7 +330,9 @@ def main():
 	    with col2:
 	 	   st.markdown("<a href='https://apextraderfunding.com/member/aff/go/itsonlymoney?c=GVXZJABB' target='_blank' style='color: white; display: block; text-align: center; width: 100%; background-color: #567BF4; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-weight: bold;'>APEX PROMO | Use 'GVXZJABB'</a>", unsafe_allow_html=True)
 
-	    plot_all_expiry(ticker)
+	    plot_max_pain(ticker,selected_option)
+	    plot_all_expiry(ticker)	    	
+
 
     st.markdown("<div style='text-align: justify; padding: 20px; border-radius: 5px; color: white;'><p> The data presented in this app is sourced from Yahoo Finance and have delays. It's recommended to use this tool after market close. Please note that if the ticker entered is not found, no graphs will appear. Also, please avoid using the $ symbol when entering the ticker symbol. There is no need to use capital letters for the ticker.The first graph will pop up fairly quickly. The second one takes a bit longer (15s to 30s).<p>Example: QQQ, SPY, TSLA, AAPL, GOOGL... </p> </p> <p>Disclaimer: The information provided on this website is for educational purposes only and should not be construed as financial advice. The content and data presented may not be accurate or complete, and should not be relied upon for making investment decisions. You should conduct your own research and consult with a qualified financial advisor before making any investment decisions. The authors and publishers of this website are not responsible for any losses that may result from the use of this information.</p></div>", unsafe_allow_html=True)
 
